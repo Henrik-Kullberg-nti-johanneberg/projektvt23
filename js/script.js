0e27cad44064db9d2733ffc1873d6f2b26a92fc8
Henrik-Kullberg-nti-johanneberg <henrik.kullberg@elev.ga.ntig.se>
2023-02-28 10:46:46 +0100

    Solved
    Problem fixat med söknings baren

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // Denna fil skall innehålla JavaScript (JS)
 
-const hamburger = document.querySelector("#hamburger")
-const nav = document.querySelector("nav");
-
-hamburger.addEventListener('doubleclick', toggleMenu)
-
-function toggleMenu() {
-    nav.classList.toggle("active")
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "hidden";
+        }
+    }
 }
