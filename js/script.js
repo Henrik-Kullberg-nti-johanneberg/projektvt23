66df7019976cdbbd6b704530cb29df1212dd7fa3
Henrik-Kullberg-nti-johanneberg <henrik.kullberg@elev.ga.ntig.se>
2023-04-02 09:53:29 +0200

    changes
    mer ändringar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Denna fil skall innehålla JavaScript (JS)
 
+
 const hamburger = document.querySelector("#hamburger");
 const nav =document.querySelector ("nav"); 
 
@@ -9,19 +10,12 @@ function toggleMenu () {
     nav.classList.toggle("active");
 }
 
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "hidden";
-        }
-    }
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
+
